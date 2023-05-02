9c8c8d5473f5bf4fac147834acbcbb111e32e010
Xalsier <81521840+Xalsier@users.noreply.github.com>
2023-05-02 02:10:42 -0400

    Simplified css, reduced lines for codex, novel

--- js/codex.js
+++ js/codex.js
@@ -1,34 +1,17 @@
 function populateSelect(yamlData, selectorId) {
   const selector = document.getElementById(selectorId);
-  // Clear the options before adding new ones
   selector.innerHTML = '';
   yamlData.items.forEach((item) => {
     const option = document.createElement('option');
     option.value = `${yamlData.path}/${item.file}`;
     option.text = item.title;
-    if (item.key) {
-      option.setAttribute('data-key', item.key);
-    }
-    selector.appendChild(option);
-  });
-}
-
+    if (item.key) {option.setAttribute('data-key', item.key);}selector.appendChild(option);});}
 function fetchAndPopulate(yamlData, selectorId, filterKey, filterValue) {
-  fetch('yaml/data.yaml')
-    .then((response) => response.text())
-    .then((yamlText) => jsyaml.load(yamlText))
-    .then((data) => {
+  fetch('yaml/data.yaml').then((response) => response.text()).then((yamlText) => jsyaml.load(yamlText)).then((data) => {
       let path, items = [];
-      if (filterKey === 'volumes') {
-        path = data.chapters.path;
-        items = data.chapters.volumes[filterValue].items;
-      } else if (filterKey === 'collections') {
-        path = data.pages.path;
-        items = data.pages.collections[filterValue].items;
-      }
-      populateSelect({ path, items }, selectorId);
-    });
-}
+      if (filterKey === 'volumes') {path = data.chapters.path;items = data.chapters.volumes[filterValue].items;
+      } else if (filterKey === 'collections') {path = data.pages.path;items = data.pages.collections[filterValue].items;}
+      populateSelect({ path, items }, selectorId);});}
 if (document.getElementById('collection-list')) {
   fetch('yaml/data.yaml')
     .then((response) => response.text())
@@ -39,52 +22,29 @@ if (document.getElementById('collection-list')) {
         const option = document.createElement('option');
         option.value = collection;
         option.text = collection;
-        document.getElementById('collection-list').appendChild(option);
-      });
-      fetchAndPopulate(data.pages, 'page-list', 'collections', collectionList[0]);
-    });
-  document.getElementById('collection-list').addEventListener('change', function () {
-    const selectedCollection = this.value;
-    fetch('yaml/data.yaml')
-      .then((response) => response.text())
-      .then((yamlText) => jsyaml.load(yamlText))
-      .then((data) => {
-        fetchAndPopulate(data.pages, 'page-list', 'collections', selectedCollection);
-      });
-  });
-}
+        document.getElementById('collection-list').appendChild(option);});
+      fetchAndPopulate(data.pages, 'page-list', 'collections', collectionList[0]);});
+  document.getElementById('collection-list').addEventListener('change', function () {const selectedCollection = this.value;
+    fetch('yaml/data.yaml').then((response) => response.text()).then((yamlText) => jsyaml.load(yamlText)).then((data) => {fetchAndPopulate(data.pages, 'page-list', 'collections', selectedCollection);});});}
 if (document.getElementById('page-list')) {
   const pageSelector = document.getElementById('page-list');
   pageSelector.addEventListener('change', function () {
     const selectedItem = this.options[this.selectedIndex];
     const filePath = selectedItem.value;
     const key = selectedItem.getAttribute('data-key');
-    displayContent('page-content', filePath, key);
-  });
+    displayContent('page-content', filePath, key);});
   pageSelector.addEventListener('DOMNodeInserted', function () {
     if (this.options.length > 0) {
       this.selectedIndex = 0;
       const filePath = this.options[0].value;
       const key = this.options[0].getAttribute('data-key');
       displayContent('page-content', filePath, key);
-      this.removeEventListener('DOMNodeInserted', arguments.callee);
-    }
-  });
-}
+      this.removeEventListener('DOMNodeInserted', arguments.callee);}});}
 function displayContent(contentId, filePath, key = null) {
-  if (key) {
-    if (!sessionStorage.getItem(key)) {
-      filePath = '';
-    }
-  }
+  if (key) {if (!sessionStorage.getItem(key)) {filePath = '';}}
   fetch(filePath)
     .then(response => response.text())
     .then(data => {
       const md = new markdownit();
       const content = document.getElementById(contentId);
-      content.innerHTML = md.render(data);
-    })
-    .catch(error => {
-      console.error('Error fetching content:', error);
-    });
-}
+      content.innerHTML = md.render(data);}).catch(error => {console.error('Error fetching content:', error);});}

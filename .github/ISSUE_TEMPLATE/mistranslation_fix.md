27d59754caa131686eba7a7cc9ddc9dd5ccf323b
Xalsier <immortalfawx@gmail.com>
2023-05-08 00:38:14 -0400

    Resolves #8, Snapshot 3.7 if bugs were fixed

---
+++ .github/ISSUE_TEMPLATE/mistranslation_fix.md
@@ -0,0 +1,24 @@
+---
+name: Translation Error
+about: Report a mistranslation or incorrect translation on the website
+title: "[Translation Error] - [Language]"
+labels: translation error
+assignees: ''
+
+---
+
+**Language:**
+Please specify the language for which you are reporting the error.
+
+**Page URL:**
+Please provide the URL of the page where you found the error.
+
+**Mistranslation:**
+Please describe the mistranslation or incorrect translation you found.
+
+**Suggested Correction:**
+Please provide your suggestion for a corrected translation.
+
+**Additional Context:**
+Please provide any additional information or context that might be helpful for fixing the error.
+
--- js/perf/footer.js
+++ js/perf/footer.js
@@ -1,6 +1,6 @@
 const footerContent = `
 <div>
-<a href="https://github.com/Xalsier/nak_snapshot" target="_blank" class="red-link">(v0.3.6.5)-snapshot</a>
+<a href="https://github.com/Xalsier/nak_snapshot" target="_blank" class="red-link">(v0.3.7)-snapshot</a>
 <p>Na'kioku © 2023 Xalsier Kitsune. All rights reserved.</p>
 <a href="https://twitter.com/Xalsier" target="_blank">
     <img src="./ass/twitter.png" alt="Twitter" width="32" height="32" loading="lazy"></a>
@@ -9,22 +9,14 @@ const footerContent = `
 </div>
 `;
 
-let footerInjected = false;
-
 function injectFooterWhenScrolled() {
-  if (!footerInjected) {
-    const windowHeight = window.innerHeight;
-    const scrollY = window.scrollY;
-    const documentHeight = document.documentElement.offsetHeight;
+  const windowHeight = window.innerHeight;
+  const scrollY = window.scrollY;
+  const documentHeight = document.documentElement.offsetHeight;
 
-    // You can adjust the 0.8 value to control when the footer is injected.
-    // 0.8 means that the footer will be injected when the user scrolls 80% of the page height.
-    if (scrollY > (documentHeight - windowHeight) * 0.8) {
-      document.getElementById('footer-placeholder').innerHTML = footerContent;
-      footerInjected = true;
-      // Remove the event listener after the footer has been injected
-      window.removeEventListener('scroll', injectFooterWhenScrolled);
-    }
+  if (scrollY > (documentHeight - windowHeight) * 0.8) {
+    document.getElementById('footer-placeholder').innerHTML = footerContent;
+    window.removeEventListener('scroll', injectFooterWhenScrolled);
   }
 }
 

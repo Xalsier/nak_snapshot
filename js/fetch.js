9e9a5cbd747727f3ddb8b3d57cad984e49a9c514
Xalsier <immortalfawx@gmail.com>
2023-05-14 16:53:40 -0400

    Made the footer a button at the bottom

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,14 @@
-fetch('../mod/footer.html')
-  .then(response => response.text())
-  .then(html => {document.getElementById('footerContainer').innerHTML = html;})
-  .catch(error => {console.error('Error fetching footer.html:', error);})
+fetch('/mod/footer.html')
+.then(response => response.text())
+.then(html => {
+   document.getElementById('footerContainer').innerHTML = html;
+   document.getElementById('help-button').addEventListener('click', function() {
+      var footer = document.querySelector('footer');
+      if (footer.style.bottom === '0px') {
+         footer.style.bottom = '-90px';
+      } else {
+         footer.style.bottom = '0px';
+      }
+   });
+})
+.catch(error => {console.error('Error fetching footer.html:', error);})
--- js/perf/bak_load.js
+++ js/perf/bak_load.js
@@ -1,52 +1,27 @@
 const bgImages = [
-  '../ass/dat/bak_res1.dat',
-  '../ass/dat/bak_res2.dat',
-  '../ass/dat/bak_res3.dat',
-  '../ass/sera_res4.jpg'
+  '../ass/sera_r0.jpg',
+  '../ass/sera_r1.jpg',
+  '../ass/sera_r2.jpg',
+  '../ass/sera_r3.jpg',
+  '../ass/sera_r4.jpg'
 ];
 const backgroundImage = document.getElementById('background-image');
-
-// Fetch the base64 strings for the dat files
-async function fetchBase64Strings() {
-  const base64Strings = [];
-  for (const src of bgImages.slice(0, 3)) {
-    const response = await fetch(src);
-    const data = await response.text();
-    base64Strings.push(data);
-  }
-  return base64Strings;
-}
-
+const delay = 500; // Adjust the delay as per your preference
 function loadImage(src) {
   return new Promise((resolve) => {
     const img = new Image();
     img.src = src;
     img.onload = () => {
+      console.log(`Image loaded: ${src}`);
       resolve();
     };
   });
 }
-
-function loadImagesSequentially(images, index) {
-  if (index >= images.length) {
-    return;
-  }
-
-  loadImage(images[index]).then(() => {
-    backgroundImage.style.backgroundImage = `url('${images[index]}')`;
-    console.log(`Image loaded: ${images[index]}`);
-    setTimeout(() => {
-      loadImagesSequentially(images, index + 1);
-    }, 500); // Adjust the delay as per your preference
-  });
-}
-
 async function loadImages() {
-  const base64Strings = await fetchBase64Strings();
-  const updatedBgImages = [...base64Strings, ...bgImages.slice(3)];
-  loadImagesSequentially(updatedBgImages, 0);
+  for (const src of bgImages) {
+    await loadImage(src);
+    backgroundImage.style.backgroundImage = `url('${src}')`;
+    await new Promise(resolve => setTimeout(resolve, delay));
+  }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-  loadImages();
-});
+document.addEventListener('DOMContentLoaded', () => {loadImages();});

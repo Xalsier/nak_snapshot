7d481492875827fc01dd3d434ebca27a4fa5f908
Xalsier <immortalfawx@gmail.com>
2023-05-15 02:26:57 -0400

    Reduced Javascript, added extra animation

--- js/perf/bak_load.js
+++ js/perf/bak_load.js
@@ -1,27 +1,10 @@
-const bgImages = [
-  '../ass/sera_r0.jpg',
-  '../ass/sera_r1.jpg',
-  '../ass/sera_r2.jpg',
-  '../ass/sera_r3.jpg',
-  '../ass/sera_r4.jpg'
-];
-const backgroundImage = document.getElementById('background-image');
-const delay = 500; // Adjust the delay as per your preference
-function loadImage(src) {
-  return new Promise((resolve) => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      console.log(`Image loaded: ${src}`);
-      resolve();
-    };
-  });
-}
-async function loadImages() {
-  for (const src of bgImages) {
-    await loadImage(src);
-    backgroundImage.style.backgroundImage = `url('${src}')`;
-    await new Promise(resolve => setTimeout(resolve, delay));
-  }
-}
-document.addEventListener('DOMContentLoaded', () => {loadImages();});
+const bgImages = ['0','1','2','3','4'].map(i=>`../ass/sera_r${i}.jpg`),
+    bgi=document.getElementById('background-image'),delay=500; 
+const loadImage=src=>new Promise(res=>{const img=new Image();
+    img.src=src;
+    img.onload=()=>(console.log(`Image loaded:${src}`),res());});
+const loadImages=async()=>{
+    for(const src of bgImages){await loadImage(src);
+        bgi.style.backgroundImage=`url('${src}')`;
+        await new Promise(r=>setTimeout(r,delay));}};
+document.addEventListener('DOMContentLoaded', loadImages);
--- js/translations.js
+++ js/translations.js
@@ -1,21 +1,11 @@
 let translations;
+
 async function loadTranslations() {
     const response = await fetch('../translation.json');
-    translations = await response.json();
-    const languageSelector = document.getElementById('language-selector');
-    if (languageSelector) {
-        languageSelector.addEventListener('change', (event) => {
-            const selectedLanguage = event.target.value;
-            console.log('Language changed to', selectedLanguage);
-            updateText(selectedLanguage, translations);
-        });
-    }
-    return translations;
+    return await response.json();
 }
 
 function updateText(language, translations) {
-    console.log('Selected Language:', language);
-    console.log('Translations:', translations);
     const elementsToTranslate = document.querySelectorAll('[data-translate]');
     elementsToTranslate.forEach(element => {
         const translationKey = element.getAttribute('data-translate');
@@ -23,16 +13,15 @@ function updateText(language, translations) {
     });
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
     const languageSelector = document.getElementById('language-selector');
     if (languageSelector) {
-        languageSelector.addEventListener('change', (event) => {
+        languageSelector.addEventListener('change', async (event) => {
             const selectedLanguage = event.target.value;
-            console.log('Language changed to', selectedLanguage);
+            if (!translations) {
+                translations = await loadTranslations();
+            }
             updateText(selectedLanguage, translations);
         });
     }
-    translations = await loadTranslations();
-    const defaultLanguage = 'ja';
-    updateText(defaultLanguage, translations);
 });

3aec45cd39c1c5f8c0c58f6a34fd65bc1c036d86
Xalsier <immortalfawx@gmail.com>
2023-05-15 17:58:10 -0400

    Reduced Javascript for Windows

--- js/translations.js
+++
@@ -1,8 +0,0 @@
-let t;
-document.addEventListener('DOMContentLoaded',async()=>{
-    let s=document.getElementById('language-selector');
-    s&&s.addEventListener('change',async e=>{
-        let l=e.target.value;
-        t=t||await(await fetch('../translation.json')).json();
-        document.querySelectorAll('[data-translate]').forEach(e=>{
-            e.innerText=t[l][e.getAttribute('data-translate')]})})})

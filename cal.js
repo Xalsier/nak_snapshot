0c871038ac4d5b7470202ce5a5757b9112c29d2d
Xalsier <immortalfawx@gmail.com>
2023-05-24 23:29:17 -0400

    Reduced dependency on fetch, adding comments

--- cal.js
+++ cal.js
@@ -1,7 +1,19 @@
+const events = {
+  "2023-01-25": {"type": 3}, // Placeholder Event
+  "2023-01-27": {"type": 3}, // Placeholder Event
+  "2023-04-16": {"type": 4}, // Placeholder Event
+  "2023-04-28": {"type": 3}, // Placeholder Event
+  "2023-04-30": {"type": 2}, // Placeholder Event
+  "2023-05-07": {"type": 5}, // Placeholder Event
+  "2023-05-13": {"type": 5}, // Placeholder Event
+  "2023-05-16": {"type": 5}, // Placeholder Event
+  "2023-05-17": {"type": 5} // Placeholder Event
+};
+// Code Golfed Calendar // < 1000 Characters //
 日 = (e, n) => new Date(e, n, 1).getDay();
 古 = (e, n, a) => new Date(e, n, a) < Date.now();
 隠 = _ => 面 && (面.innerHTML = "");
-獲 = _ => fetch("eve.json").then(e => e.json()).then(e => e.events);
+獲 = _ => events; // Calendar data is already available locally (No need for Fetch API)
 設 = (e, n) => {(文 = 書[找]("cmy")) && (文.innerHTML = 12 * e + n + 1);};
 造 = (e, n, a, s) => {
   (d = 書[元]("div")),
@@ -11,7 +23,7 @@
     Array.from({ length: n }, (_, n) => n + 1).forEach(n => {
       let o = 書[元]("div"),
         c = e[`${a}-${(s + 1 + "").padStart(2, "0")}-${(n + "").padStart(2, "0")}`],
-        t = new Date(a, s, n).toDateString() == new Date().toDateString();
+        t = new Date(a, s, n).toDateString() == 初.toDateString();
       o[名] = `cd ${古(a, s, n) ? "past " + `past-event-${c?.type}` : `event-${c?.type}`} ${t ? "today" : ""}`,
       o.innerHTML = n,
       d.appendChild(o);
@@ -20,21 +32,20 @@
 };
 面 = null;
 初 = new Date();
-元 = "createElement";
-名 = "className";
-找 = "getElementById";
-旧 = (書 = document)[找]("pm");
-新 = 書[找]("nm");
+元 = "createElement"; // DOM Element Variable
+名 = "className"; // DOM Element Variable
+找 = "getElementById"; // DOM Element Variable
+旧 = (書 = document)[找]("pm"); // Previous Month
+新 = 書[找]("nm"); // Next Month
 (更 = e => {
   初.setMonth(初.getMonth() + e);
   年 = 初.getFullYear();
   月 = 初.getMonth();
   数 = new Date(年, 月 + 1, 0).getDate();
   設(年, 月);
-  獲().then(e => {
-    (根 = 書[找]("ca")).innerHTML = "";
-    根.appendChild(造(e, 数, 年, 月));
-  });
+  根 = 書[找]("ca");
+  根.innerHTML = "";
+  根.appendChild(造(獲(), 数, 年, 月));
 })(0);
 旧.onclick = _ => 更(-1);
 新.onclick = _ => 更(1);

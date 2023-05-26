d989666323f10a46ad57c2de15234362a99f4743
Xalsier <81521840+Xalsier@users.noreply.github.com>
2023-05-26 18:41:00 -0400

    Update trv.js

--- js/trv.js
+++ js/trv.js
@@ -1,7 +1,13 @@
 var trivia = [
-    "#1. Leo's zodiac sign is.. well.. Leo.",
-    "#2. The current moon is 24544. On earth that would be 2045!",
-    "#3. Earth Julian date has already been calculated for ya!",
-  ];
-var randomTrivia = trivia[Math.floor(Math.random() * trivia.length)];
-document.getElementById('warning-text').innerHTML = `<p>${randomTrivia}</p>`;
+    "Leo's zodiac sign is.. well.. Leo.",
+    "The current moon year is 24544. Converted to Earth years, that would be 2045!",
+    "The Earth Julian date has already been calculated for you!",
+];
+function getRandomTrivia() {
+    return trivia[Math.floor(Math.random() * trivia.length)];
+}
+function displayTrivia() {
+    var triviaElement = document.getElementById('warning-text');
+    triviaElement.textContent = getRandomTrivia();
+}
+displayTrivia();

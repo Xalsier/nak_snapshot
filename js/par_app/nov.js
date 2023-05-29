443ef262b92b21eb4860b2fc6c906b58791590cd
Xalsier <immortalfawx@gmail.com>
2023-05-29 07:00:29 -0400

    Added word count and Chapter Select

--- js/par_app/nov.js
+++ js/par_app/nov.js
@@ -1,44 +1,46 @@
 export const novel = {
   "Chapter 1": [
-    {
-      "page": 1,
-      "content": [
-        "Please ignore this I'm trying to get the webnovel parser to work",
-        "Pellentesque euismod massa et scelerisque lobortis.",
-        "Nullam pretium urna nec risus cursus vestibulum.",
-        "In gravida mi nec ante ultrices, sed efficitur tortor faucibus.",
-        "Vivamus feugiat mauris nec gravida finibus."
-      ]
-    },
-    {
-      "page": 2,
-      "content": [
-        "Suspendisse fringilla nisl in metus feugiat ultricies.",
-        "Maecenas commodo tortor a nunc varius, eget cursus sapien porttitor.",
-        "Morbi lobortis sem vel tellus lobortis lobortis.",
-        "Etiam sagittis nisi at nisl blandit, vel tristique neque interdum.",
-        "Duis id purus euismod, tincidunt urna id, pellentesque urna."
-      ]
-    },
-    {
-      "page": 3,
-      "content": [
-        "Proin a ligula vestibulum, hendrerit ipsum vel, dapibus mi.",
-        "Nunc facilisis ex non tempor cursus.",
-        "Phasellus sed lacus sagittis, convallis lectus ac, posuere justo.",
-        "Praesent ultrices sapien in magna hendrerit luctus.",
-        "Vestibulum sodales sapien id tortor consectetur, vitae pulvinar urna tincidunt."
-      ]
-    },
-    {
-      "page": 4,
-      "content": [
-        "Fusce et odio in ex euismod feugiat.",
-        "Aliquam consectetur enim nec lobortis pellentesque.",
-        "Cras hendrerit odio sed ante dictum, sit amet hendrerit velit porttitor.",
-        "Sed sit amet ligula ut tortor venenatis ultricies.",
-        "Donec eleifend enim ut vestibulum consectetur."
-      ]
-    }
+    "Please ignore this I'm trying to get the webnovel parser to work",
+    "Pellentesque euismod massa et scelerisque lobortis.",
+    "Nullam pretium urna nec risus cursus vestibulum.",
+    "In gravida mi nec ante ultrices, sed efficitur tortor faucibus.",
+    "Vivamus feugiat mauris nec gravida finibus.",
+    "Suspendisse fringilla nisl in metus feugiat ultricies.",
+    "Maecenas commodo tortor a nunc varius, eget cursus sapien porttitor.",
+    "Morbi lobortis sem vel tellus lobortis lobortis.",
+    "Etiam sagittis nisi at nisl blandit, vel tristique neque interdum.",
+    "Duis id purus euismod, tincidunt urna id, pellentesque urna.",
+    "Proin a ligula vestibulum, hendrerit ipsum vel, dapibus mi.",
+    "Nunc facilisis ex non tempor cursus.",
+    "Phasellus sed lacus sagittis, convallis lectus ac, posuere justo.",
+    "Praesent ultrices sapien in magna hendrerit luctus.",
+    "Vestibulum sodales sapien id tortor consectetur, vitae pulvinar urna tincidunt.",
+    "Fusce et odio in ex euismod feugiat.",
   ],
+  "Chapter 2": [
+    "Aliquam consectetur enim nec lobortis pellentesque.",
+    "Cras hendrerit odio sed ante dictum, sit amet hendrerit velit porttitor.",
+    "Sed sit amet ligula ut tortor venenatis ultricies.",
+    "Donec eleifend enim ut vestibulum consectetur.",
+    "Please ignore this I'm trying to get the webnovel parser to work",
+    "Pellentesque euismod massa et scelerisque lobortis.",
+    "Nullam pretium urna nec risus cursus vestibulum.",
+    "In gravida mi nec ante ultrices, sed efficitur tortor faucibus.",
+    "Vivamus feugiat mauris nec gravida finibus.",
+    "Suspendisse fringilla nisl in metus feugiat ultricies.",
+    "Maecenas commodo tortor a nunc varius, eget cursus sapien porttitor.",
+    "Morbi lobortis sem vel tellus lobortis lobortis.",
+    "Etiam sagittis nisi at nisl blandit, vel tristique neque interdum.",
+    "Duis id purus euismod, tincidunt urna id, pellentesque urna.",
+    "Proin a ligula vestibulum, hendrerit ipsum vel, dapibus mi.",
+    "Nunc facilisis ex non tempor cursus.",
+    "Phasellus sed lacus sagittis, convallis lectus ac, posuere justo.",
+    "Praesent ultrices sapien in magna hendrerit luctus.",
+    "Vestibulum sodales sapien id tortor consectetur, vitae pulvinar urna tincidunt.",
+    "Fusce et odio in ex euismod feugiat.",
+    "Aliquam consectetur enim nec lobortis pellentesque.",
+    "Cras hendrerit odio sed ante dictum, sit amet hendrerit velit porttitor.",
+    "Sed sit amet ligula ut tortor venenatis ultricies.",
+    "Donec eleifend enim ut vestibulum consectetur.",
+  ]
 };
--- js/par_app/par.js
+++ js/par_app/par.js
@@ -1,45 +1,43 @@
 import { novel } from './nov.js';
-let currentPage = 1;
-let currentChapter = "Chapter 1";
-let totalPages = novel[currentChapter].length;
-const chapterContainer = document.createElement('div');
+const state = {
+  currentChapter: "Chapter 1",
+};
+const chapterContainer = document.getElementById('dark-overlay').appendChild(document.createElement('div'));
 chapterContainer.className = 'chapter';
-document.getElementById('dark-overlay').appendChild(chapterContainer);
-
-const pageNumberContainer = document.createElement('div');
-pageNumberContainer.className = 'page-number';
-document.getElementById('dark-overlay').appendChild(pageNumberContainer);
-
-function renderPage() {
-  chapterContainer.innerHTML = "";
-  const chapterHeader = document.createElement('h2');
-  chapterHeader.textContent = currentChapter;
-  chapterContainer.appendChild(chapterHeader);
-  const page = novel[currentChapter][currentPage - 1];
-  
-  for (const paragraph of page.content) {
-    const para = document.createElement('p');
-    para.innerHTML = paragraph;
-    chapterContainer.appendChild(para);
-    const emptyLine = document.createElement('br');
-    chapterContainer.appendChild(emptyLine);
-  }
-  
-  pageNumberContainer.textContent = `Page ${currentPage} of ${totalPages}`;
+function createElement(tag, text) {
+  const element = document.createElement(tag);
+  element.textContent = text;
+  return element;
 }
-
-document.getElementById('prev-button').addEventListener('click', () => {
-  if (currentPage > 1) {
-    currentPage--;
-    renderPage();
+function renderContent() {
+  const currentChapter = state.currentChapter;
+  const paragraphs = novel[currentChapter];
+  chapterContainer.innerHTML = '';
+  const wordCount = createElement('div', `Word Count: ${getWordCount(paragraphs)}`);
+  wordCount.style.float = 'right';
+  chapterContainer.appendChild(wordCount);
+  const chapterSelect = createElement('select');
+  chapterSelect.addEventListener('change', (event) => {
+    state.currentChapter = event.target.value;
+    renderContent();
+  });
+  for (const chapter in novel) {
+    const option = createElement('option', chapter);
+    option.value = chapter;
+    option.selected = chapter === currentChapter;
+    chapterSelect.appendChild(option);
   }
-});
-
-document.getElementById('next-button').addEventListener('click', () => {
-  if (currentPage < totalPages) {
-    currentPage++;
-    renderPage();
-  }
-});
-
-renderPage();
+  chapterContainer.appendChild(chapterSelect);
+  const chapterHeader = createElement('h2', currentChapter);
+  chapterContainer.appendChild(chapterHeader);
+  paragraphs.forEach(paragraph => {
+    chapterContainer.appendChild(createElement('p', paragraph));
+  });
+}
+function getWordCount(paragraphs) {
+  return paragraphs.reduce((count, paragraph) => {
+    const words = paragraph.trim().split(' ');
+    return count + words.length;
+  }, 0);
+}
+renderContent();

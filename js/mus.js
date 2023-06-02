cb75e15696c6b17f2db888f4e02bf38a95792658
Xalsier <immortalfawx@gmail.com>
2023-06-01 22:44:35 -0400

    Simplified mus.js

--- js/mus.js
+++ js/mus.js
@@ -1,63 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var calendarToggle = document.getElementById("cal-toggle");
-    var darkOverlayToggle = document.getElementById("dark-mode-toggle");
-    var footerToggle = document.getElementById("toggle-footer");
-    var audio = document.getElementById("background-music");
-    var startTime = 1;
-    var isPlaying = false;
-    var fadeOutInterval;
-    var fadeInInterval;
-    var storedTime = 0;
-    var delayTimer;
-
-    function toggleFooter() {
-      var calendarChecked = calendarToggle.checked;
-      var darkOverlayChecked = darkOverlayToggle.checked;
-      footerToggle.checked = !(calendarChecked || darkOverlayChecked);
-    
-      // Clear the previous timer if it exists
-      if (delayTimer) {
-        clearTimeout(delayTimer);
-      }
-    
-      if (isPlaying) {
-        fadeOutAudio();
-      } else {
-        // Start a new delay timer
-        delayTimer = setTimeout(function() {
-          // Check if the state has changed
-          if (!calendarToggle.checked && !darkOverlayToggle.checked) {
-            fadeInAudio();
-          }
-        }, 2000);  // Adjust this delay to the value you want
-      }
-    }
-    function fadeOutAudio() {
-      clearInterval(fadeInInterval);
-      fadeOutInterval = setInterval(function() {
-        audio.volume = Math.max(audio.volume - 0.05, 0);
-        if (audio.volume === 0) {
-          clearInterval(fadeOutInterval);
-          audio.pause();
-          isPlaying = false;
-        }
-      }, 100);
-    }
-    function fadeInAudio() {
-      clearInterval(fadeOutInterval);
-      audio.currentTime = storedTime || startTime;
-      audio.play();
-      isPlaying = true;
-      fadeInInterval = setInterval(function() {
-        audio.volume = Math.min(audio.volume + 0.05, 1);
-        if (audio.volume === 1) {
-          clearInterval(fadeInInterval);
-        }
-      }, 100);
-    }
-    calendarToggle.addEventListener("change", toggleFooter);
-    darkOverlayToggle.addEventListener("change", toggleFooter);
-    audio.addEventListener("timeupdate", function() {
-      storedTime = audio.currentTime;
-    });
-  });
+const calendarToggle = document.getElementById("cal-toggle");
+const darkOverlayToggle = document.getElementById("dark-mode-toggle");
+const footerToggle = document.getElementById("toggle-footer");
+const audio = document.getElementById("background-music");
+const startTime = 1;
+let isPlaying = false;
+let fadeOutInterval;
+let fadeInInterval;
+let storedTime = 0;
+let delayTimer;
+const toggleFooter = () => {
+  const calendarChecked = calendarToggle.checked;
+  const darkOverlayToggleChecked = darkOverlayToggle.checked;
+  footerToggle.checked = !(calendarChecked || darkOverlayToggleChecked);
+  delayTimer ? clearTimeout(delayTimer) : null;
+  isPlaying ? fadeOutAudio() : delayTimer = setTimeout(() => {
+  (!calendarToggle.checked && !darkOverlayToggle.checked) ? fadeInAudio() : null;}, 2000);}
+const fadeOutAudio = () => {
+  clearInterval(fadeInInterval);
+  fadeOutInterval = setInterval(() => {
+    audio.volume = Math.max(audio.volume - 0.05, 0);
+    audio.volume === 0 ? (clearInterval(fadeOutInterval), audio.pause(), isPlaying = false) : null;}, 100);} 
+const fadeInAudio = () => {
+  clearInterval(fadeOutInterval);
+  audio.currentTime = storedTime || startTime;
+  audio.play();
+  isPlaying = true;
+  fadeInInterval = setInterval(() => {
+    audio.volume = Math.min(audio.volume + 0.05, 1);
+    audio.volume === 1 ? clearInterval(fadeInInterval) : null;}, 100);}  
+calendarToggle.addEventListener("change", toggleFooter);
+darkOverlayToggle.addEventListener("change", toggleFooter);
+audio.addEventListener("timeupdate", () => {storedTime = audio.currentTime;});
